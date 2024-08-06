db5189b9884c72079d8c4588deb579077de8c3d2
solo-leveler <shinichiizumi@protonmail.ch>
2024-08-06 15:41:06 +0530

    added bishop logic file

---
+++ src/app/chess-logic/pieces/bishop.ts
@@ -0,0 +1,18 @@
+import { FENChar, Coords, Color } from '../model';
+import { Piece } from './piece';
+
+export class Bishop extends Piece {
+  protected override _FENChar: FENChar;
+  protected override _directions: Coords[] = [
+    { x: 1, y: 1 },
+    { x: 1, y: -1 },
+    { x: -1, y: 1 },
+    { x: -1, y: -1 },
+  ];
+
+  constructor(private pieceColor: Color) {
+    super(pieceColor);
+    this._FENChar =
+      pieceColor === Color.White ? FENChar.WhiteBishop : FENChar.BlackBishop;
+  }
+}
