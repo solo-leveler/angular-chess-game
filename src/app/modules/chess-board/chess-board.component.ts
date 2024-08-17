d64290790f8c6023cb623c4fd3102effc410355d
solo-leveler <shinichiizumi@protonmail.ch>
2024-08-17 13:12:17 +0530

    create chessboard

--- src/app/modules/chess-board/chess-board.component.ts
+++ src/app/modules/chess-board/chess-board.component.ts
@@ -4,13 +4,15 @@ import { Color, FENChar } from '../../chess-logic/model';
 
 @Component({
   selector: 'app-chess-board',
-  standalone: true,
-  imports: [],
   templateUrl: './chess-board.component.html',
   styleUrl: './chess-board.component.css'
 })
 export class ChessBoardComponent {
    private chessBoard = new ChessBoard();
-   private chessBoardView: (FENChar | null)[][] = this.chessBoard.chessBoardView;
+   public chessBoardView: (FENChar | null)[][] = this.chessBoard.chessBoardView;
    public get playerColor(): Color { return this.chessBoard.playerColor; };
+
+   public isSquareDark(x : number, y :number ):boolean{
+      return ChessBoard.isSquareDark(x,y);  
+   } 
 }
