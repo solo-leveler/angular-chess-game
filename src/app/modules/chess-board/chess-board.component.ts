bf761e2dd75adcce6b4a3589cff9b74545b91455
solo-leveler <shinichiizumi@protonmail.ch>
2024-08-20 22:30:35 +0530

    adding piece image path

--- src/app/modules/chess-board/chess-board.component.ts
+++ src/app/modules/chess-board/chess-board.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ChessBoard } from '../../chess-logic/chess-board';
-import { Color, FENChar } from '../../chess-logic/model';
+import { Color, FENChar, pieceImagePaths } from '../../chess-logic/model';
 
 @Component({
   selector: 'app-chess-board',
@@ -8,6 +8,7 @@ import { Color, FENChar } from '../../chess-logic/model';
   styleUrl: './chess-board.component.css'
 })
 export class ChessBoardComponent {
+   public pieceImagePaths = pieceImagePaths;
    private chessBoard = new ChessBoard();
    public chessBoardView: (FENChar | null)[][] = this.chessBoard.chessBoardView;
    public get playerColor(): Color { return this.chessBoard.playerColor; };
