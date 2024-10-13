9309a38b6193339028a66930f5731d0781e68296
solo-leveler <shinichiizumi@protonmail.ch>
2024-10-13 18:32:14 +0530

    implementing safeposition method

--- src/app/chess-logic/chess-board.ts
+++ src/app/chess-logic/chess-board.ts
@@ -9,6 +9,7 @@ import { Rook } from './pieces/rook';
 
 export class ChessBoard {
   private chessBoard: (Piece | null)[][];
+  private readonly chessBoardSize: number = 8;
   private _playerColor = Color.White;
 
   constructor() {
@@ -60,7 +61,7 @@ export class ChessBoard {
     ];
   }
 
-  public get playerColor() {
+  public get playerColor(): Color {
     return this._playerColor;
   }
 
@@ -74,5 +75,77 @@ export class ChessBoard {
 
   public static isSquareDark(x: number, y: number): boolean {
     return (x % 2 == 0 && y % 2 == 0) || (x % 2 == 1 && y % 2 == 1);
+  }
+
+  private areCoordsValid(x: number, y: number): boolean {
+    return (
+      x >= 0 && y >= 0 && x < this.chessBoardSize && y < this.chessBoardSize
+    );
+  }
+  public isInCheck(playerColor: Color): boolean {
+    for (let x = 0; x < this.chessBoardSize; x++) {
+      for (let y = 0; y < this.chessBoardSize; y++) {
+        const piece: Piece | null = this.chessBoard[x][y];
+        if (!piece || piece.color == playerColor) continue;
+        for (const { x: dx, y: dy } of piece.directions) {
+          let newX: number = x + dx;
+          let newY: number = y + dy;
+
+          if (!this.areCoordsValid(newX, newY)) continue;
+
+          if (
+            piece instanceof Pawn ||
+            piece instanceof Knight ||
+            piece instanceof King
+          ) {
+            //pawns attack diagonal only
+            if (piece instanceof Pawn && dy == 0) continue;
+            const attackedPiece: Piece | null = this.chessBoard[newX][newY];
+            if (
+              attackedPiece instanceof King &&
+              attackedPiece.color === playerColor
+            )
+              return true;
+          } else {
+            while (this.areCoordsValid(newX, newY)) {
+              const attackedPiece: Piece | null = this.chessBoard[newX][newY];
+              if (
+                attackedPiece instanceof King &&
+                attackedPiece.color === playerColor
+              )
+                return true;
+
+              if (attackedPiece !== null) break;
+
+              newX += dx;
+              newY += dy;
+            }
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  private isPosSafeAfterMove(
+    piece: Piece,
+    prevX: number,
+    prevY: number,
+    newX: number,
+    newY: number
+  ) {
+    const newPiece: Piece | null = this.chessBoard[newX][newY];
+    //same color piece already on the square
+    if (newPiece && newPiece.color === piece.color) return false;
+    //simulate position
+    this.chessBoard[prevX][prevY] = null;
+    this.chessBoard[newX][newY] = piece;
+
+    const isPositionSafe: boolean = !this.isInCheck(piece.color);
+
+    this.chessBoard[prevX][prevY] = piece;
+    this.chessBoard[newX][newY] = newPiece;
+
+    return isPositionSafe;
   }
 }
