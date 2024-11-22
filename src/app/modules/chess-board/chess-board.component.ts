fedbddcd0de4d31a902612dcb3a75fa659719ed3
solo-leveler <shinichiizumi@protonmail.ch>
2024-11-22 22:56:50 +0530

    implementing method that marks selected piece and its safe squares

--- src/app/modules/chess-board/chess-board.component.ts
+++ src/app/modules/chess-board/chess-board.component.ts
@@ -1,19 +1,51 @@
 import { Component } from '@angular/core';
 import { ChessBoard } from '../../chess-logic/chess-board';
-import { Color, FENChar, pieceImagePaths } from '../../chess-logic/model';
+import {
+  Color,
+  Coords,
+  FENChar,
+  pieceImagePaths,
+  SafeSquares,
+} from '../../chess-logic/model';
+import { SelectedSqaure } from './model';
 
 @Component({
   selector: 'app-chess-board',
   templateUrl: './chess-board.component.html',
-  styleUrl: './chess-board.component.css'
+  styleUrl: './chess-board.component.css',
 })
 export class ChessBoardComponent {
-   public pieceImagePaths = pieceImagePaths;
-   private chessBoard = new ChessBoard();
-   public chessBoardView: (FENChar | null)[][] = this.chessBoard.chessBoardView;
-   public get playerColor(): Color { return this.chessBoard.playerColor; };
+  public pieceImagePaths = pieceImagePaths;
+  private chessBoard = new ChessBoard();
+  public chessBoardView: (FENChar | null)[][] = this.chessBoard.chessBoardView;
+  public get playerColor(): Color {
+    return this.chessBoard.playerColor;
+  }
+  public get safeSqaures(): SafeSquares {
+    return this.chessBoard.safeSqaures;
+  }
 
-   public isSquareDark(x : number, y :number ):boolean{
-      return ChessBoard.isSquareDark(x,y);  
-   } 
+  private selectedSqaure: SelectedSqaure = { piece: null };
+  private pieceSafeSqaures: Coords[] = [];
+
+  public isSquareDark(x: number, y: number): boolean {
+    return ChessBoard.isSquareDark(x, y);
+  }
+
+  public isSqaureSelected(x: number, y: number): boolean {
+    if (!this.selectedSqaure.piece) return false;
+    return this.selectedSqaure.x === x && this.selectedSqaure.y === y;
+  }
+
+  public isSqaureSafeForSelectedPiece(x: number, y: number): boolean {
+    return this.pieceSafeSqaures.some((coords) => coords.x === x && coords.y===y);
+  }
+
+  public selectingPiece(x: number, y: number): void {
+    const piece: FENChar | null = this.chessBoardView[x][y];
+    if (!piece) return;
+
+    this.selectedSqaure = { piece, x, y };
+    this.pieceSafeSqaures = this.safeSqaures.get(x + ',' + y) || [];
+  }
 }
